17ce76ecba50b76c7cb29ea18afabd3dd25ad95e
integritychain <eschorn@integritychain.com>
2023-10-30 14:39:03 -0500

    front matter

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,21 @@
+use crate::algs::check;
+
+mod algs;
+
 pub fn add(left: usize, right: usize) -> usize {
+    check();
     left + right
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::algs::check;
     use super::*;
 
     #[test]
     fn it_works() {
         let result = add(2, 2);
+        check();
         assert_eq!(result, 4);
     }
 }
